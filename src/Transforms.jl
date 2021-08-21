7f5f3c02c92afda61a168e9638edba66d6599c57
tudor <tudor.pahomi@gmail.com>
2021-08-21 23:09:31 +0200

    minor

--- src/Transforms.jl
+++ src/Transforms.jl
@@ -731,10 +731,10 @@ end
 closest_to_dw = ProcessData(
 
 				function just_slice(P::AbstractDict, A::AbstractArray{<:Number, N};
-														R::Number, inds::AbstractVector{Int}, dim::Int 
+														R::Number, inds::AbstractVector{Int}, kwargs... 
 														)::Tuple{AbstractArray{<:Number,N}, String} where N
 
-								(selectdim(A, dim, inds), dist_dw_label(R))
+				(selectdim(A, N==1 ? 1 : kwargs[:dim], inds), dist_dw_label(R))
 
 
 				end)
