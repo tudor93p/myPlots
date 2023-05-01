a631c71d80f1d9bc6afe8942bcf02fe19df19912
tudor <tudor.pahomi@gmail.com>
2023-05-01 06:36:30 +0200

    update (ran script)

--- pyplots/VectorField_atE.py
+++ pyplots/VectorField_atE.py
@@ -6,7 +6,7 @@ from VectorField import nr_axes, plot
 from VectorField import common_sliders as common_sliders0
 
 
-common_sliders = common_sliders0 + [choose_energy, energy_zoom]
+common_sliders = common_sliders0 + [choose_energy]
 
 
 
